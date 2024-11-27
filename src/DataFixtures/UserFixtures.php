84c94391d4d28354baab418c9d44d6a8121314e7
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2024-11-27 16:03:37 +0100

    ajout de la fixture de User avec création de comptes ADMIN et USER

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        UserFactory::createOne([
+            'firstname' => 'Come',
+            'lastname' => 'PEYRELONGUE',
+            'login' => 'Come',
+            'roles' => ['ROLE_ADMIN'],
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'firstname' => 'Hugo',
+            'lastname' => 'RAYAUME',
+            'login' => 'Hugo',
+            'roles' => ['ROLE_ADMIN'],
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'firstname' => 'Enzo',
+            'lastname' => 'DUPONT',
+            'login' => 'Enzo',
+            'roles' => ['ROLE_ADMIN'],
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'firstname' => 'Thomas',
+            'lastname' => 'LINDEN',
+            'login' => 'ThomasL',
+            'roles' => ['ROLE_ADMIN'],
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'firstname' => 'Thomas',
+            'lastname' => 'PAZZE',
+            'login' => 'ThomasP',
+            'roles' => ['ROLE_ADMIN'],
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'firstname' => 'firstname',
+            'lastname' => 'lastname',
+            'login' => 'user',
+            'roles' => ['ROLE_USER'],
+            'password' => 'test',
+        ]);
+
+        for ($i = 0; $i < 10; ++$i) {
+            UserFactory::createOne([
+                'roles' => ['ROLE_USER'],
+            ]);
+        }
+
+        $manager->flush();
+    }
+}
