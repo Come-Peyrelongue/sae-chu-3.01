fc0f298cd6756f15393f0c82f57e21e8fe119def
Côme <come.peyrelongue@etudiant.univ-reims.fr>
2024-12-19 23:58:37 +0100

    Fin d'implémentation globale des fonctionnalités des professionnels : calendrier avec Full Calendar OK, affichage des séances : OK, modification : OK, création OK, suppression PAS OK -> Problème avec la méthode DELETE

--- src/Controller/ProfessionnelController.php
+++ src/Controller/ProfessionnelController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfessionnelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfessionnelController extends AbstractController
 {
     #[Route('/professionnel', name: 'app_professionnel')]
-    public function index(): Response
+    public function index(
+        Security $security,
+        ProfessionnelRepository $professionnelRepository
+    ): Response
     {
+        $login = $security->getUser()->getLogin();
+        $pro = $professionnelRepository->findOneBylogin($login);
+
         return $this->render('professionnel/index.html.twig', [
+            'pro' => $pro,
             'controller_name' => 'ProfessionnelController',
         ]);
     }
