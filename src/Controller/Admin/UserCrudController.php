9922fa00ff3a043520da5c48cdbca25fa9bdac5d
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2025-01-09 16:58:51 +0100

    modification du mdp d'un user depuis l'espace admin possible

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,14 +3,37 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPasssword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPasssword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -32,6 +55,23 @@ class UserCrudController extends AbstractCrudController
                             return '';
                     }
                 }),
+            TextField::new('password')
+                ->onlyOnForms()
+                ->setFormType(PasswordType::class)
+                ->setRequired(false)
+                ->setEmptyData('')
+                ->setFormTypeOption('attr', ['autocomplete' => 'new-password']),
+
         ];
     }
+
+    public function setUserPasssword(User $user): void
+    {
+        $password = $this->getContext()->getRequest()->request->all()['User']['password'];
+        if ('' != $password) {
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+            $user->setPassword($hashedPassword);
+        }
+    }
+
 }
